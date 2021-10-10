792d9d5c56655157dd049d9912242f12d1940c7f
naofal <naofal.nirvana@yahoo.com>
2021-10-10 21:49:26 +0700

    Initial Commit

---
+++ frontend/scripts/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const cartButtons = document.querySelectorAll(".cart-button");
+
+  cartButtons.forEach((button) => {
+    button.addEventListener("click", cartClick);
+  });
+
+  function cartClick() {
+    let button = this;
+    button.classList.add("clicked");
+  }
+});
